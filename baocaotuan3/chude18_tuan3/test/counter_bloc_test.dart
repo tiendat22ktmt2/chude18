ac55c327aa66f1b57bfc1c630811b9b180501e56
tiendat22ktmt2 <132873857+tiendat22ktmt2@users.noreply.github.com>
2025-11-06 11:54:51 +0700

    Cap nhat lan 3

--- baocaotuan3/chude18_tuan3/test/counter_bloc_test.dart
+++ baocaotuan3/chude18_tuan3/test/counter_bloc_test.dart
@@ -6,27 +6,55 @@ import 'package:chude18_tuan3/bloc/counter_state.dart';
 import 'package:chude18_tuan3/data/counter_repository.dart';
 
 void main() {
+  TestWidgetsFlutterBinding.ensureInitialized();
   group('CounterBloc', () {
-    late CounterRepository repository;
-    late CounterBloc bloc;
 
-    setUp(() {
-      repository = CounterRepository();
-      bloc = CounterBloc(repository);
-    });
-
+    //Kiểm thử event IncrementPressed
     blocTest<CounterBloc, CounterState>(
       'emits [1] when IncrementPressed is added',
-      build: () => bloc,
+      build: () => CounterBloc(CounterRepository()),
       act: (bloc) => bloc.add(IncrementPressed()),
-      expect: () => [const CounterState(value: 1, isMax: false, isMin: false)],
+      expect: () => [
+        const CounterState(value: 1, isMax: false, isMin: false),
+      ],
     );
+
+
+    //Kiểm thử event DecrementPressed
 
     blocTest<CounterBloc, CounterState>(
       'emits [-1] when DecrementPressed is added',
-      build: () => bloc,
+      build: () => CounterBloc(CounterRepository()),
       act: (bloc) => bloc.add(DecrementPressed()),
-      expect: () => [const CounterState(value: -1, isMax: false, isMin: false)],
+      expect: () => [
+        const CounterState(value: -1, isMax: false, isMin: false),
+      ],
+    );
+
+    //Kiểm thử event ResetPressed
+    blocTest<CounterBloc, CounterState>(
+      'resets counter to min value when ResetPressed is added',
+      build: () => CounterBloc(CounterRepository()),
+      act: (bloc) {
+        bloc.add(IncrementPressed());
+        bloc.add(IncrementPressed());
+        bloc.add(ResetPressed());
+      },
+      expect: () => [
+        const CounterState(value: 1, isMax: false, isMin: false),
+        const CounterState(value: 2, isMax: false, isMin: false),
+        const CounterState(value: -5, isMax: false, isMin: true),
+      ],
+    );
+
+    //Kiểm thử SetLimitPressed (thay đổi min/max)
+    blocTest<CounterBloc, CounterState>(
+      'updates limits when SetLimitPressed is added',
+      build: () => CounterBloc(CounterRepository()),
+      act: (bloc) => bloc.add(SetLimitPressed(-2, 5)),
+      expect: () => [
+        const CounterState(value: -2, isMax: false, isMin: true),
+      ],
     );
   });
 }
